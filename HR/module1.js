496aa1f96f562c433615813b3e61b3b9e5b26457
George Lopez-Ayala <georgelopezayala@icloud.com>
2017-05-12 13:00:50 -0700

    started module1

---
+++ HR/module1.js
@@ -0,0 +1,21 @@
+// Write a function called "isOldEnoughToDrink".
+// Given a number, in this case an age, "isOldEnoughToDrink" returns whether a person of this given age is old enough to legally drink in the United States.
+
+function isOldEnoughToDrink(age) {
+  // your code here
+  return age >= 21 ? true : false;
+}
+
+// Write a function called "isOldEnoughToDrive".
+// Given a number, in this case an age, "isOldEnoughToDrive" returns whether a person of this given age is old enough to legally drive in the United States.
+function isOldEnoughToDrive(age) {
+  // your code here
+  return age >= 16 ? true : false;
+}
+
+// Given a number, in this case an age, 'isOldEnoughToVote' returns whether
+// a person of this given age is old enough to legally vote in the United States.
+function isOldEnoughToVote(age) {
+  // your code here
+  return age >= 18 ? true : false;
+}
